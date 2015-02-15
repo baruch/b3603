d8b0aa836bc0f27601e3e6763463cbd5439f3234
Baruch Even <baruch@ev-en.org>
2015-02-15 17:59:57 +0200

    Define a protocol and start to implement it

---
+++ stm8/PROTOCOL.md
@@ -0,0 +1,152 @@
+# Serial Protocol
+
+This is a description of the serial protocol for this alternative firmware.
+
+## Configuration
+
+The serial works at 9600 8N1, the low speed is intended to allow MCU to handle
+the flow without interfering too much with the rest of the work.
+
+## Startup
+
+The controller will send a welcome message "B3603 alternative firmware vX.XX"
+
+## Commands
+
+All commands are line-feed or carriage-return terminated, if the input buffer
+is filled with no LF or CR character the entire line is dumped and a message is
+sent on the output to indicate this failure.
+
+### Model Query
+
+* Send: "MODEL"
+* Receive: "MODEL: B3603"
+
+This is just the model information, it is fixed for this firmware at this time
+as it only runs on a single device. There is however another device that seems
+to use the same controller board and then the model returned may be different.
+
+### Version Query
+
+* Send: "VERSION"
+* Receive: "VERSION: X.XX"
+
+### Name Query
+
+* Send: "NAME"
+* Receive: "NAME: <name>"
+
+Allow the user to name the device in case several are connected to the same
+machine, this can be used for discovery and fine control in a larger system.
+
+### Name Set
+
+* Send: "SNAME"
+* Receive: "SNAME: <name>"
+
+Set the name to what the user gave. The size is limited to 16 characters and
+they must be printable characters.
+
+### Voltage Capabilities Query
+
+* Send: "VLIST"
+* Receive: "VLIST: 1.0000/12.0000/0.0001"
+
+Returns minimum voltage, maximum voltage and step size.
+
+### Current Capabilities Query
+
+* Send: "CLIST"
+* Receive: "CLIST: 0.001/3.000/0.001"
+
+Returns minimum current, maximum current and step size.
+
+### Output Enable/Disable
+
+* Send: "OUTPUT 0" or "OUTPUT 1"
+* Receive: "OUTPUT: DISABLED" or "OUTPUT: ENABLED"
+
+OUTPUT0 disables the output and OUTPUT1 enables the output.
+
+### Voltage Set
+
+* Send: "VSET X.XXXX"
+* Receive: "VOLTAGE: SET X.XXXX"
+
+Set the maximum voltage level.
+
+### Current Set
+
+* Send: "CSET X.XXXX"
+* Receive: "CURRENT: SET X.XXXX"
+
+Set the maximum current level.
+
+### Default at startup
+
+* Send: "DEFAULT 0" or "DEFAULT 1"
+* Receive: "DEFAULT: DISABLED" or "DEFAULT: ENABLED"
+
+Set the default for the output at startup, either enabled or disabled.
+
+Default disabled is better for safety, default enabled is useful when the unit
+powers an always on device and needs to always work without a manual
+intervention when the power comes back after a power outage.
+
+### Over voltage protection
+
+* Send: "VSHUTDOWN X.XXXX" or "VSHUTDOWN 0"
+* Receive: "VSHUTDOWN: X.XXXX" or "VSHUTDOWN: DISABLED"
+
+When a VSHUTDOWN is set and reached the unit will turn off the output to avoid
+an over-voltage situation.
+
+### Over current protection
+
+* Send: "CSHUTDOWN 1" or "CSHUTDOWN 0"
+* Receive: "CSHUTDOWN: ENABLED" or "CSHUTDOWN: DISABLED"
+
+When a CSHUTDOWN is set and reached the unit will shutdown, current overload is
+considered when we reach the maximum current and the voltage drops by 10%
+already to handle the current control, at that point it is assumed the load is
+in short and the output is shutdown.
+
+### Query configuration
+
+* Send: "CONFIG"
+* Receive: "CONFIG: Output/Voutmax/Ioutmax/Vshutdown/Cshutdown"
+
+Report all the config variables:
+
+* Output -- Output enabled "1" or disabled "0"
+* Voutmax -- Voltage output maximum
+* Ioutmax -- Current output max as set
+* Vshutdown -- Voltage set for shutdown, or "0" for feature disabled
+* Cshutdown -- Current shutdown enabled "1" or disabled "0"
+
+### Status Report
+
+* Send: "STATUS"
+* Receive: "STATUS: Output/Vin/Vout/Iout/CCCV"
+
+Reports all state variables:
+
+* Output -- Output enabled "1" or disabled "0"
+* Vin -- Voltage Input to the unit
+* Voutmax -- Voltage output maximum
+* Vout -- Actual voltage output
+* Ioutmax -- Current output max as set
+* Iout -- Actual current output
+* CCCV -- "1" if we are in constant current, "0" if we are in constant voltage
+* Vshutdown -- Voltage set for shutdown, or "0" for feature disabled
+* Cshutdown -- Current shutdown enabled "1" or disabled "0"
+
+## Missing features
+
+* Calibration
+* Lock keys
+* Save settings to EEPROM, also load them at startup
+* Watchdog, can we output something on the serial?
+* Internal notification logic, monitor the output values and notify immediately
+  on non-trivial change (i.e. apply hysteresis so minutiae errors are not
+  noisy)
--- stm8/main.c
+++ stm8/main.c
@@ -1,15 +1,158 @@
-#define STM8S003 1
+#define FW_VERSION "0.0.1"
+
 #include "stm8l.h"
 #include <string.h>
 #include <stdint.h>
+#include <ctype.h>
 
-void uart_write(const char *str)
+uint8_t uart_write_buf[64];
+uint8_t uart_write_start;
+uint8_t uart_write_len;
+
+uint8_t uart_read_buf[64];
+uint8_t uart_read_len;
+uint8_t read_newline;
+
+uint8_t cfg_name[17];
+
+uint8_t uart_write_ready(void)
 {
-	char i;
-	for(i = 0; str[i] != 0; i++) {
-		while(!(USART1_SR & USART_SR_TXE));
-		USART1_DR = str[i];
+	return (USART1_SR & USART_SR_TXE);
+}
+
+uint8_t uart_read_available(void)
+{
+	return (USART1_SR & USART_SR_RXNE);
+}
+
+void uart_write_ch(const char ch)
+{
+	USART1_DR = ch;
+}
+
+void uart_flush_write()
+{
+	while(!(USART1_SR & USART_SR_TXE));
+}
+
+uint8_t uart_write_free_space(void)
+{
+	return sizeof(uart_write_buf) - uart_write_len;
+}
+
+void uart_write_str(const char *str)
+{
+	uint8_t i;
+
+	// Move the buffer to the start
+	if (uart_write_start > 0) {
+		for (i = 0; i < uart_write_len; i++) {
+			uart_write_buf[i] = uart_write_buf[i+uart_write_start];
+		}
+		uart_write_start = 0;
 	}
+
+	for(i = 0; str[i] != 0 && uart_write_len < sizeof(uart_write_buf); i++) {
+		uart_write_buf[uart_write_len] = str[i];
+		uart_write_len++;
+	}
+}
+
+void uart_write_from_buf(void)
+{
+	if (uart_write_len > 0 && uart_write_ready()) {
+		uart_write_ch(uart_write_buf[uart_write_start]);
+		uart_write_start++;
+		uart_write_len--;
+
+		if (uart_write_len == 0)
+			uart_write_start = 0;
+	}
+}
+
+uint8_t uart_read_ch(void)
+{
+	return USART1_DR;
+}
+
+void uart_read_to_buf(void)
+{
+	// Don't read if we are writing
+	if (uart_write_len > 0 || !uart_write_ready())
+		return;
+
+	if (uart_read_available()) {
+		uint8_t ch = uart_read_ch();
+		uart_read_buf[uart_read_len] = ch;
+		uart_read_len++;
+
+		if (ch == '\r' || ch == '\n')
+			read_newline = 1;
+
+		// Empty the read buf if we are overfilling and there is no full command in there
+		if (uart_read_len == sizeof(uart_read_buf) && !read_newline) {
+			uart_read_len = 0;
+			uart_write_str("READ OVERFLOW\r\n");
+		}
+	}
+}
+
+void set_name(uint8_t *name)
+{
+	uint8_t idx;
+
+	for (idx = 0; name[idx] != 0; idx++) {
+		if (!isprint(name[idx]))
+			name[idx] = '.'; // Eliminate non-printable chars
+	}
+
+	strncpy(cfg_name, name, sizeof(cfg_name));
+	cfg_name[sizeof(cfg_name)-1] = 0;
+
+	uart_write_str("SNAME: ");
+	uart_write_str(cfg_name);
+	uart_write_str("\r\n");
+}
+
+void process_input()
+{
+	// Eliminate the CR/LF character
+	uart_read_buf[uart_read_len-1] = 0;
+
+	if (strcmp(uart_read_buf, "MODEL") == 0) {
+		uart_write_str("MODEL: B3606\r\n");
+	} else if (strcmp(uart_read_buf, "VERSION") == 0) {
+		uart_write_str("VERSION: " FW_VERSION "\r\n");
+	} else if (strcmp(uart_read_buf, "NAME") == 0) {
+		uart_write_str("NAME: ");
+		uart_write_str(cfg_name);
+		uart_write_str("\r\n");
+	} else {
+		// Process commands with arguments
+		uint8_t idx;
+		uint8_t space_found = 0;
+
+		for (idx = 0; idx < uart_read_len; idx++) {
+			if (uart_read_buf[idx] == ' ') {
+				uart_read_buf[idx] = 0;
+				space_found = 1;
+				break;
+			}
+		}
+
+		if (space_found) {
+			if (strcmp(uart_read_buf, "SNAME") == 0) {
+				set_name(uart_read_buf + idx + 1);
+			} else {
+				uart_write_str("UNKNOWN COMMAND!\r\n");
+			}
+		} else {
+			uart_write_str("UNKNOWN COMMAND\r\n");
+		}
+	}
+
+	uart_read_len = 0;
+	read_newline = 0;
 }
 
 void clk_init()
@@ -20,17 +163,43 @@ void clk_init()
 
 void uart_init()
 {
-	PD_DDR = 0x10; // Put TX line on
-	PD_CR1 = 0x10;
-
 	USART1_CR1 = 0; // 8 bits, no parity
     USART1_CR3 &= ~(USART_CR3_STOP1 | USART_CR3_STOP2); // 1 stop bit
 
 	USART1_BRR2 = 0x00;
 	USART1_BRR1 = 0x0D; // 9600 baud, order important between BRRs, BRR1 must be last
 
-//	USART1_CR2 = USART_CR2_TEN | UART_CR2_REN; // Allow TX & RX
-	USART1_CR2 = USART_CR2_TEN; // Allow TX
+	USART1_CR2 = USART_CR2_TEN | USART_CR2_REN; // Allow TX & RX
+
+	uart_write_len = 0;
+	uart_write_start = 0;
+	uart_read_len = 0;
+	read_newline = 0;
+}
+
+void pinout_init()
+{
+	// PA1 is 74HC595 SHCP, output
+	// PA2 is 74HC595 STCP, output
+	PA_DDR = (1<<1) | (1<<2);
+
+	// PB4 is Enable control, output
+	// PB5 is CV/CC sense, input
+	PB_DDR = (1<<4);
+
+	// PC4 is Iout sense, input
+	// PC5 is Vout control, output
+	// PC6 is Iout control, output
+	PC_DDR = (5<<1) || (6<<1);
+
+	// PD2 is Vout sense, input
+	// PD3 is Vin sense, input
+	PD_DDR = 0;
+}
+
+void config_load(void)
+{
+	strcpy(cfg_name, "Unnamed");
 }
 
 int main()
@@ -39,9 +208,18 @@ int main()
 
 	clk_init();
 	uart_init();
+	pinout_init();
+
+	config_load();
+
+	uart_write_str("B3606 starting\r\n");
 
 	do {
-		uart_write("Hello World!\r\n");
-		for(i = 0; i < 147456; i++) { } // Sleep
+		uart_write_from_buf();
+		uart_read_to_buf();
+		
+		if (read_newline) {
+			process_input();
+		}
 	} while(1);
 }

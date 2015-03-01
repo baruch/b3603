71fbe997a8c070c87141c194944802b2aa7fd445
Baruch Even <baruch@ev-en.org>
2015-03-01 21:40:45 +0200

    Use rounding in multiplication for accuracy

--- stm8/fixedpoint.c
+++ stm8/fixedpoint.c
@@ -1,12 +1,24 @@
 #include "fixedpoint.h"
 
+/* For better accuracy we round to the nearest number, either up or down.
+ * We save one extra bit temporarily and if it is 1 we round up and if it is 0
+ * we round down.
+ */
 uint16_t fixed_mult(uint16_t x, uint16_t y)
 {
 	uint32_t tmp;
+	uint8_t round;
+	uint16_t tmp16;
 
 	tmp = x;
 	tmp *= y;
-	tmp >>= FIXED_SHIFT;
+	tmp >>= FIXED_SHIFT-1;
 
-	return tmp;
+	round = tmp&1;
+	tmp16 = tmp >> 1;
+
+	if (round)
+		return tmp16+1;
+	else
+		return tmp16;
 }

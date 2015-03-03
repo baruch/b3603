135ad771b577a6ff20e5e49d544be53effb9f783
Baruch Even <baruch@ev-en.org>
2015-03-03 15:32:44 +0200

    Use memcpy instead of manually loading variables
    This saves space on config reset and also allows for a return to
    defaults with ease.

--- stm8/config.c
+++ stm8/config.c
@@ -30,45 +30,41 @@
 
 #define DEFAULT_NAME_STR "Unnamed"
 
+cfg_system_t default_cfg_system = {
+	.version = SYSTEM_CFG_VERSION,
+	.name = "Unnamed",
+	.default_on = 0,
+	.output = 0,
+	.autocommit = 1,
+
+	.vin_adc = { .a = FLOAT_TO_FIXED(16*3.3), .b = 0 },
+	.vout_adc = { .a = FLOAT_TO_FIXED(3.3/0.073), .b = FLOAT_TO_FIXED(0.452) },
+	.cout_adc = { .a = FLOAT_TO_FIXED(3.3*1.25), .b = FLOAT_TO_FIXED(0.2) },
+	.vout_pwm = { .a = FLOAT_TO_FIXED(0.073), .b = FLOAT_TO_FIXED(0.033) },
+	.cout_pwm = { .a = FLOAT_TO_FIXED(0.8), .b = FLOAT_TO_FIXED(0.160) },
+};
+
+cfg_output_t default_cfg_output = {
+	OUTPUT_CFG_VERSION,
+	FLOAT_TO_FIXED(5), // 5V
+	FLOAT_TO_FIXED(0.5), // 0.5A
+	0,
+	0,
+};
+
 inline void validate_system_config(cfg_system_t *sys)
 {
-	if (sys->version != SYSTEM_CFG_VERSION) {
-		memset(sys, 0, sizeof(*sys));
-		sys->version = SYSTEM_CFG_VERSION;
+	if (sys->version != SYSTEM_CFG_VERSION ||
+			sys->name[0]  ||
+			sys->vin_adc.a  ||
+			sys->vout_adc.a  ||
+			sys->cout_adc.a  ||
+			sys->vout_pwm.a  ||
+			sys->cout_pwm.a
+			) {
+		memcpy(sys, &default_cfg_system, sizeof(default_cfg_system));
 		// TODO: If we want easy upgradability we can implement it here to
 		// upgrade from an old struct to a new one.
-	}
-
-	if (sys->name[0] == 0) {
-		memcpy(sys->name, DEFAULT_NAME_STR, sizeof(DEFAULT_NAME_STR));
-		sys->default_on = 0;
-		sys->output = 0;
-		sys->autocommit = 1;
-	}
-
-	if (sys->vin_adc.a == 0) {
-		sys->vin_adc.a = FLOAT_TO_FIXED(16*3.3);
-		sys->vin_adc.b = 0;
-	}
-
-	if (sys->vout_adc.a == 0) {
-		sys->vout_adc.a = FLOAT_TO_FIXED(3.3/0.073);
-		sys->vout_adc.b = FLOAT_TO_FIXED(0.452);
-	}
-
-	if (sys->cout_adc.a == 0) {
-		sys->cout_adc.a = FLOAT_TO_FIXED(3.3*1.25);
-		sys->cout_adc.b = FLOAT_TO_FIXED(0.2);
-	}
-
-	if (sys->vout_pwm.a == 0) {
-		sys->vout_pwm.a = FLOAT_TO_FIXED(0.073);
-		sys->vout_pwm.b = FLOAT_TO_FIXED(0.033);
-	}
-
-	if (sys->cout_pwm.a == 0) {
-		sys->cout_pwm.a = FLOAT_TO_FIXED(0.8);
-		sys->cout_pwm.b = FLOAT_TO_FIXED(0.160);
 	}
 }
 
@@ -90,10 +86,7 @@ void config_save_system(cfg_system_t *sys)
 inline void validate_output_config(cfg_output_t *cfg)
 {
 	if (cfg->version != OUTPUT_CFG_VERSION || cfg->vset == 0 || cfg->cset == 0) {
-		cfg->vset = FLOAT_TO_FIXED(5); // 5V
-		cfg->cset = FLOAT_TO_FIXED(0.5); // 0.5A
-		cfg->vshutdown = 0;
-		cfg->cshutdown = 0;
+		memcpy(cfg, &default_cfg_output, sizeof(default_cfg_output));
 	}
 }
 

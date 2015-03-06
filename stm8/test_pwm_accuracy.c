8211b75ab50959e6876b1c6299a4a8c7810acbbc
Baruch Even <baruch@ev-en.org>
2015-03-06 20:15:17 +0200

    Adapt test_pwm_accuracy to new format
    We now use mV as source

--- stm8/test_pwm_accuracy.c
+++ stm8/test_pwm_accuracy.c
@@ -6,7 +6,6 @@ void uart_write_str(const char *s) { (void)s; }
 
 #include "fixedpoint.c"
 #include "outputs.c"
-#include "parse.c"
 #include "eeprom.c"
 #include "config.c"
 
@@ -14,32 +13,15 @@ void uart_write_str(const char *s) { (void)s; }
 
 int main()
 {
-	uint8_t str[10];
 	uint16_t val;
 	uint16_t pwm;
-	int digit1, digit2, digit3;
 	cfg_system_t sys;
 
 	config_load_system(&sys);
 
-	str[0] = '0';
-	str[1] = '.';
-	str[2] = '0';
-	str[3] = '0';
-	str[4] = 0;
-
-	for (digit1 = '0'; digit1 <= '9'; digit1++) {
-		str[0] = digit1;
-		for (digit2 = '0'; digit2 <= '9'; digit2++) {
-			str[2] = digit2;
-			for (digit3 = '0'; digit3 <= '9'; digit3++) {
-				str[3] = digit3;
-
-				val = parse_fixed_point(str);
-				pwm = pwm_from_set(val, &(sys.vout_pwm));
-				printf("%s %u %u\n", str, val, pwm);
-			}
-		}
+	for (val = 10; val < 35000; val+=10) {
+		pwm = pwm_from_set(val, &(sys.vout_pwm));
+		printf("%u %u.%03u %u %.2f\n", val, val/1000, val % 1000, pwm, ((double)pwm)/8191.0*100.0);
 	}
-return 0;
+	return 0;
 }

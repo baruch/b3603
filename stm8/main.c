9fdd645c14ba132953b6e853ac890de4fe3d0604
Baruch Even <baruch@ev-en.org>
2015-03-22 18:51:51 +0200

    Re-enable commented out messages
    This saved space but I do want the messages, will find other way to save
    space.

--- stm8/main.c
+++ stm8/main.c
@@ -90,7 +90,7 @@ void autocommit(void)
 void set_output(uint8_t *s)
 {
 	if (s[1] != 0) {
-//		uart_write_str("OUTPUT takes either 0 for OFF or 1 for ON, received: \"");
+		uart_write_str("OUTPUT takes either 0 for OFF or 1 for ON, received: \"");
 		uart_write_str(s);
 		uart_write_str("\"\r\n");
 		return;
@@ -103,7 +103,7 @@ void set_output(uint8_t *s)
 		cfg_system.output = 1;
 		uart_write_str("OUTPUT: ON\r\n");
 	} else {
-//		uart_write_str("OUTPUT takes either 0 for OFF or 1 for ON, received: \"");
+		uart_write_str("OUTPUT takes either 0 for OFF or 1 for ON, received: \"");
 		uart_write_str(s);
 		uart_write_str("\"\r\n");
 	}
@@ -546,7 +546,7 @@ void ensure_afr0_set(void)
 	if ((OPT2 & 1) == 0) {
 		uart_flush_writes();
 		if (eeprom_set_afr0()) {
-//			uart_write_str("AFR0 set, reseting the unit\r\n");
+			uart_write_str("AFR0 set, reseting the unit\r\n");
 			uart_flush_writes();
 			iwatchdog_init();
 			while (1); // Force a reset in a few msec
